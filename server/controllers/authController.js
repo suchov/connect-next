96146b09872b40b0a7df071b16240abadacd327a
Artem Sychov <artem.suchov@gmail.com>
2019-08-14 15:25:30 +0300

    signin functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
+const passport = require("passport");
 
 exports.validateSignup = (req, res, next) => {
   req.sanitizeBody("name");
@@ -44,7 +45,24 @@ exports.signup = async (req, res) => {
   });
 };
 
-exports.signin = () => {};
+exports.signin = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return res.status(500).json(err.message);
+    }
+    if (!user) {
+      return res.status(400).json(info.message);
+    }
+
+    req.login(user, err => {
+      if (err) {
+        return res.status(500).json(err.message);
+      }
+
+      res.json(user);
+    });
+  })(req, res, next);
+};
 
 exports.signout = () => {};
 

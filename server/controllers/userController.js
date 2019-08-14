3ee6abcc4c548790ed41c9fd80e5f398275c21ae
Artem Sychov <artem.suchov@gmail.com>
2019-08-14 17:05:42 +0300

    getting the authenticated users data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,7 +6,15 @@ exports.getUsers = async (req, res) => {
   res.json(user);
 };
 
-exports.getAuthUser = () => {};
+exports.getAuthUser = (req, res) => {
+  if (!req.isAuthUser) {
+    res.status(403).json({
+      message: "You are unauthenticated. Please sign in or sign up."
+    });
+    return res.redirect("/signin");
+  }
+  res.json(req.user);
+};
 
 exports.getUserById = async (req, res, next, id) => {
   const user = await User.findOne({ _id: id });

416da561b327062e4f05d69a1ad649a512046dc1
Artem Sychov <artem.suchov@gmail.com>
2019-08-16 15:26:13 +0300

    implement following user functionality

--- server/routes/index.js
+++ server/routes/index.js
@@ -28,6 +28,20 @@ router.get("/api/auth/signout", authController.signout);
  */
 router.param("userId", userController.getUserById);
 
+// follow and unfollow user routes
+router.put(
+  "/api/users/follow",
+  authController.checkAuth,
+  catchErrors(userController.addFollowing),
+  catchErrors(userController.addFollower)
+);
+router.put(
+  "/api/users/unfollow",
+  authController.checkAuth,
+  catchErrors(userController.deleteFollowing),
+  catchErrors(userController.deleteFollower)
+);
+
 router
   .route("/api/users/:userId")
   .get(userController.getAuthUser)
@@ -45,19 +59,6 @@ router.get(
   "/api/users/feed/:userId",
   authController.checkAuth,
   catchErrors(userController.getUserFeed)
-);
-
-router.put(
-  "/api/users/follow",
-  authController.checkAuth,
-  catchErrors(userController.addFollowing),
-  catchErrors(userController.addFollower)
-);
-router.put(
-  "/api/users/unfollow",
-  authController.checkAuth,
-  catchErrors(userController.deleteFollowing),
-  catchErrors(userController.deleteFollower)
 );
 
 /**
